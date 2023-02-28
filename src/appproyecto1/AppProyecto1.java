04c6be3a3ff6afcf0823eb280a75987b5be7d37c
Brandon Conejo Rodríguez <branjcr14@gmail.com>
2023-02-28 17:15:34 -0600

    Actualización 2

--- src/appproyecto1/AppProyecto1.java
+++ src/appproyecto1/AppProyecto1.java
@@ -3,10 +3,10 @@ package appproyecto1;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
+
 public class AppProyecto1 {
 
     static Scanner scanner = new Scanner(System.in); //Sirve para recoger texto por consola
-    
 
     //Arreglos
     ArrayList<Integer> numeroDePago = new ArrayList<>();
@@ -24,8 +24,8 @@ public class AppProyecto1 {
     ArrayList<Double> montoDeducido = new ArrayList<>();
 
     //varible contadora
-    int conta = 0;
-
+    int conta = 1;
+    
     public static void main(String[] args) {
         new AppProyecto1().menu();
     }
@@ -54,14 +54,52 @@ public class AppProyecto1 {
                         RealizarPagos();
                         break;
                     case 3:
-                        
+                        ConsultarPagos();
+                        break;
                     case 4:
                         break;
                     case 5:
                         break;
                     case 6:
+                        SubMenu();
                         break;
                     case 7:
+                        break;
+
+                }
+            } catch (Exception e) {
+                System.err.println("Opps! un error" + e);
+            }
+
+        }
+    }
+
+    public void SubMenu() {
+
+        int select = -1;
+
+        while (select != 0) {
+
+            try {
+                System.out.println("************************\nElige opción:"
+                        + "\n1.-Ver todos los Pagos"
+                        + "\n2.-Ver Pagos por tipo de Servicio\n"
+                        + "3.-Ver Pagos por código de caja\n"
+                        + "4.-Ver Dinero Comisionado por servicios\n"
+                        + "5.-Regresar Menú Principal\n");
+                select = Integer.parseInt(scanner.nextLine());
+                switch (select) {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+                    case 3:
+
+                    case 4:
+                        break;
+                    case 5:
+                        menu();
                         break;
 
                 }
@@ -120,7 +158,7 @@ public class AppProyecto1 {
         apellido2.add(scanner.next());
 
         //numero de caja
-        int random = (int) (Math.random() * 3 + 1 );
+        int random = (int) (Math.random() * 3 + 1);
         numeroDeCaja.add(random);
         System.out.println("caja: " + numeroDeCaja.get(conta));
 
@@ -136,7 +174,6 @@ public class AppProyecto1 {
             }
         } while (servicioIngresado > 3 || servicioIngresado < 0);
 
-        
         System.out.println("Ingrese el número de factura: ");
         numeroDeFactura.add(scanner.next());
 
@@ -146,6 +183,7 @@ public class AppProyecto1 {
         CalcularComision();
 
         conta++; //variable contadora que sirve de Quía para la posición de los arreglos.
+        
 
     }
 
@@ -166,6 +204,26 @@ public class AppProyecto1 {
         }
         montoComision.add(comision);
     }
-    
-    
+
+    public void ConsultarPagos() {
+        System.out.println("**** Consulta de pagos ****");
+        System.out.println("Ingrese el número de pago a consultar");
+        Integer numeroPagoAConsultar = Integer.valueOf(scanner.nextLine());
+
+        for (int i = 0; i < numeroDePago.size(); i++) {
+            if (numeroPagoAConsultar == numeroDePago.get(i)) {
+                ImprimirDatos(i);
+            } else {
+                System.err.println("Pago no encontrado!");
+
+            }
+        }
+    }
+
+    public void ImprimirDatos( int i ) {
+        System.out.println("**** Impresión de datos ****");
+        System.out.println("-Numero de pago: " + numeroDePago.get(i) );
+        
+    }
+
 }
